b29c057e6b6564eea70de8bb90a0805cbfe27ff0
ExclusiveKlotz <72287094+28klotlucas2@users.noreply.github.com>
2021-11-14 03:23:10 +0000

    Cool styling.

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def page():
     print(request.form['name'] + " said " + request.form['contents'] + "!")
     file = open("index.html", "r")
     output = file.read()
-    return output + "<p>Done! Check your console.</p>"
+    return output + """<p><span style="font-family: 'Trebuchet MS', Helvetica, sans-serif;">Done! Check your console.</span></p>"""
   else:
     file = open("index.html", "r")
     output = file.read()
-    return output
+    return output + """<p><span style="font-family: 'Trebuchet MS', Helvetica, sans-serif;">This is an example of get and post requests in Python Flask.</span></p>"""
 
 def run():
   app.run(host="0.0.0.0", port=443)
