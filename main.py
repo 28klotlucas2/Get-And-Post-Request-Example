1f90a885f7c44fdaec498920bedd2223131abc8c
28klotlucas <72287094+28klotlucas2@users.noreply.github.com>
2021-11-14 02:51:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/', methods=['GET', 'POST'])
+def page():
+  if request.method == 'POST':
+    print(request.form['name'] + " said " + request.form['contents'] + "!")
+    file = open("index.html", "r")
+    output = file.read()
+    return output + "<p>Done! Check console.</p>"
+  else:
+    file = open("index.html", "r")
+    output = file.read()
+    return output
+
+def run():
+  app.run(host="0.0.0.0", port=443)
+
+server = Thread(target=run)
+server.start()
+os.system("clear")
